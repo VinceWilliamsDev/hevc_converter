cf3b8e3637527bb755d788fd15b51c29995ed560
Vince Williams <vince.williams.dev@gmail.com>
2024-02-03 16:18:24 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,81 @@
+# convert video files to the H.265 codec
+from sys import argv
+from pathlib import Path
+import subprocess
+
+
+def main(args):
+    if len(args) == 1:
+        pwd = Path.cwd()
+        selector(pwd)
+    elif len(args) == 2:
+        arg = Path(args[1])
+        if not arg.exists():
+            raise Exception("File or directory not found")
+        if arg.is_file():
+            dest = make_destination_dir(arg.parent)
+            converter(arg, dest)
+        elif arg.is_dir():
+            selector(arg)
+        else:
+            exit(0)
+    elif len(args) > 2:
+        raise Exception('Please input only one "1" file or directory')
+    else:
+        exit(0)
+
+
+def selector(target):
+    selection = input(f'Would you like to convert all files in {target}? y/N ')
+    if selection == "y":
+        dest = make_destination_dir(target)
+        files = [f for f in Path().iterdir() if f.is_file()]
+        for f in files:
+            converter(f, dest)
+    else:
+        exit(0)
+
+
+def make_destination_dir(directory):
+    destination = directory.joinpath('hevc')
+    if not destination.exists():
+        try:
+            destination.mkdir()
+        except FileNotFoundError:
+            print('Unable to create destination directory')
+            exit(1)
+        except FileExistsError:
+            return destination
+    return destination
+
+
+def converter(file, dest):
+    if file.suffix in ['.mp4', '.mkv', '.avi', '.mov', '.wmv']:
+        input_file = file.name
+        new_name = f'{file.stem}.mp4'
+        output_file = dest.joinpath(new_name)
+
+        print(f'Starting {input_file} ')
+        print('')
+        # print(f'input_file: {input_file}')
+        # print(f'new name: {new_name}')
+        # print(f'output_file: {output_file}')
+
+        try:
+            result = subprocess.run(
+                ['ffmpeg', '-i', input_file, '-metadata', 'title=""', '-c:v', 'hevc', '-c:a', 'copy', output_file],
+                stdout=subprocess.PIPE, check=True)
+            print(result.stdout.decode())
+        except subprocess.CalledProcessError as e:
+            print(f'Command {e.cmd} failed with error {e.returncode}')
+
+        print('')
+        print(f'Finished {input_file}')
+        print('')
+    else:
+        print(f'{file.name} cannot be converted')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main(argv)
