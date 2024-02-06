96751f3a5d8efbd9a09629e6c066720ae7a066f6
Vince Williams <vince.williams.dev@gmail.com>
2024-02-06 17:29:57 -0500

    program now tracks execution duration

--- main.py
+++ main.py
@@ -11,24 +11,33 @@ from shutil import move
 
 
 def main(args: list[str]) -> None:
+    start_time: str = strftime("%Y-%m-%d %H:%M:%S", localtime())
+    end_time: str = strftime("%Y-%m-%d %H:%M:%S", localtime())
     if len(args) == 0:
         pwd: Path = Path.cwd()
         files, dest = selector(pwd)
         for file in files:
-            converter(file, dest)
+            end_time = converter(file, dest)
+        elapsed = time_elapsed(start_time, end_time)
         print('File conversions are complete\n')
+        print(f'Time elapsed: {elapsed}')
     elif len(args) == 1:
         arg: Path = Path(args[0])
         if not arg.exists():
             raise Exception("File or directory not found")
         if arg.is_file():
             dest: Path = make_destination_dir(arg.parent)
-            converter(arg, dest)
+            end_time = converter(arg, dest)
+            elapsed = time_elapsed(start_time, end_time)
+            print('File conversions are complete\n')
+            print(f'Time elapsed: {elapsed}')
         elif arg.is_dir():
             files, dest = selector(arg)
             for file in files:
-                converter(file, dest)
+                end_time = converter(file, dest)
+            elapsed = time_elapsed(start_time, end_time)
             print('File conversions are complete\n')
+            print(f'Time elapsed: {elapsed}')
         else:
             exit(0)
     elif len(args) > 1:
@@ -60,7 +69,25 @@ def make_destination_dir(directory: Path) -> Path:
     return destination
 
 
-def converter(src: Path, dest: Path) -> None:
+def time_elapsed(start: str, end: str) -> str:
+    start_list: list[str] = start.split(' ')
+    end_list: list[str] = end.split(' ')
+    delta: list[int] = []
+    for idx, interval in enumerate(end_list):
+        delta.append(int(interval) - int(start_list[idx]))
+    elapsed = f'{delta[4]} minutes, {delta[5]} seconds'
+    if delta[0] > 0:
+        elapsed = f'{delta[0]} years, {delta[1]} months, {delta[2]} days, {delta[3]} hours, {elapsed}'
+    elif delta[1] > 0:
+        elapsed = f'{delta[1]} months, {delta[2]} days, {delta[3]} hours, {elapsed}'
+    elif delta[2] > 0:
+        elapsed = f'{delta[2]} days, {delta[3]} hours, {elapsed}'
+    elif delta[3] > 0:
+        elapsed = f'{delta[3]} hours, {elapsed}'
+    return elapsed
+
+
+def converter(src: Path, dest: Path) -> str:
     log_file: Path = src.parent.joinpath('HEVC.log')
     if src.suffix in ['.mp4', '.mkv', '.avi', '.mov', '.wmv']:
         input_file: str = src.name
@@ -84,16 +111,17 @@ def converter(src: Path, dest: Path) -> None:
                         with open(log_file, 'a') as log:
                             log.write(f'[{end_time}] successfully moved ({input_file})\n')
                         print(f'{end_time}: FINISHED {input_file}\n')
-                        return
+                        return end_time
                     except FileExistsError:
                         end_time: str = strftime("%Y-%m-%d %H:%M:%S", localtime())
                         with open(log_file, 'a') as log:
                             log.write(f'[{end_time}] FAILED TO MOVE ({src.name})\n')
                         print(f'\n{end_time}: {src.name} cannot be moved\n')
-                        exit(1)
+                        return end_time
         except subprocess.CalledProcessError as e:
             end_time: str = strftime("%Y-%m-%d %H:%M:%S", localtime())
             print(f'{end_time}: Command {e.cmd} failed with error {e.returncode}\nOutput: {e.output}')
+            return end_time
 
         # if it isn't already HEVC, re-encode
         try:
@@ -105,6 +133,7 @@ def converter(src: Path, dest: Path) -> None:
             with open(log_file, 'a') as log:
                 log.write(f'[{end_time}] successfully converted ({input_file})\n')
             print(f'{end_time}: FINISHED {input_file}\n')
+            return end_time
         except subprocess.CalledProcessError as e:
             end_time: str = strftime("%Y-%m-%d %H:%M:%S", localtime())
             print(f'{end_time}: Command {e.cmd} failed with error {e.returncode}')
@@ -112,12 +141,14 @@ def converter(src: Path, dest: Path) -> None:
                 log.write(f'[{end_time}] CONVERSION FAILED FOR ({input_file})\n')
             if output_file.exists():
                 remove(output_file)
+            return end_time
 
     else:
         end_time: str = strftime("%Y-%m-%d %H:%M:%S", localtime())
         with open(log_file, 'a') as log:
             log.write(f'[{end_time}] CANNOT CONVERT ({src.name})\n')
         print(f'\n{end_time}: {src.name} cannot be converted\n')
+        return end_time
 
 
 if __name__ == '__main__':
