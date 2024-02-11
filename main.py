cbec6b5d43e48829de3c644b44726269974eb9e0
Vince Williams <vince.williams.dev@gmail.com>
2024-02-10 22:27:18 -0500

    fixed bug on line 168 - log_file no longer in scope

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from shutil import move
 def main(args: list[str]) -> None:
     start_time: datetime = datetime.now()
     end_time: datetime = datetime.now()
+
     if len(args) == 0:
         pwd: Path = Path.cwd()
         files, dest = selector(pwd)
@@ -23,16 +24,19 @@ def main(args: list[str]) -> None:
         elapsed = time_elapsed(start_time, end_time)
         print('File conversions are complete')
         print(f'Time elapsed: {elapsed}\n')
+
     elif len(args) == 1:
         arg: Path = Path(args[0])
         if not arg.exists():
             raise Exception("File or directory not found")
+
         if arg.is_file():
             dest: Path = make_destination_dir(arg.parent)
             end_time = converter(arg, dest)
             elapsed = time_elapsed(start_time, end_time)
             print('File conversions are complete')
             print(f'Time elapsed: {elapsed}\n')
+
         elif arg.is_dir():
             files, dest = selector(arg)
             for file in files:
@@ -40,8 +44,7 @@ def main(args: list[str]) -> None:
             elapsed = time_elapsed(start_time, end_time)
             print('File conversions are complete')
             print(f'Time elapsed: {elapsed}\n')
-        else:
-            exit(0)
+
     elif len(args) > 1:
         raise Exception('Please input only one(1) file or directory')
     else:
@@ -50,6 +53,7 @@ def main(args: list[str]) -> None:
 
 def selector(target: Path) -> tuple[list[Path], Path]:
     selection: str = input(f'Would you like to convert all files in {target}? y/N ')
+
     if selection.lower() == ("y" or "yes"):
         dest: Path = make_destination_dir(target)
         files: list[Path] = [f for f in target.iterdir() if f.is_file()]
@@ -60,6 +64,7 @@ def selector(target: Path) -> tuple[list[Path], Path]:
 
 def make_destination_dir(directory: Path) -> Path:
     destination: Path = directory.joinpath('hevc')
+
     if not destination.exists():
         try:
             destination.mkdir()
@@ -89,8 +94,10 @@ def time_elapsed(start: datetime, end: datetime) -> str:
 
 def log_event(timestamp: datetime, directory: Path, target: str, event_type: str) -> None:
     log_file: Path = directory.joinpath('HEVC.log')
+
     print(
         f'[{timestamp.date()} {timestamp.hour}:{timestamp.minute}:{timestamp.second}] {event_type} ({target})\n')
+
     try:
         with open(log_file, 'a') as log:
             log.write(
@@ -143,6 +150,7 @@ def converter(src: Path, dest: Path) -> datetime:
                 ['ffmpeg', '-i', input_file, '-metadata', 'title=', '-c:v', 'hevc', '-c:a', 'copy', output_file],
                 stdout=subprocess.PIPE, check=True)
             print(result.stdout.decode())
+
             end_time = datetime.now()
             log_event(end_time, src.parent, input_file, 'successfully converted')
             return end_time
@@ -150,13 +158,14 @@ def converter(src: Path, dest: Path) -> datetime:
             end_time = datetime.now()
             print(f'[{end_time.date()} {end_time.hour}:{end_time.minute}:{end_time.second}] Command {e.cmd} failed with error {e.returncode}')
             log_event(end_time, src.parent, input_file, 'CONVERSION FAILED FOR')
+
             if output_file.exists():
                 remove(output_file)
             return end_time
 
     else:
         end_time = datetime.now()
-        if src.name not in [dest.name, log_file.name]:
+        if src.name not in ['hevc', 'HEVC.log']:
             log_event(end_time, src.parent, src.name, 'CANNOT CONVERT')
         return end_time
 
