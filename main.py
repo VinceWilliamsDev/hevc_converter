2bad952eff9eb6748959457056fa17f97f4f1a64
Vince Williams <vince.williams.dev@gmail.com>
2024-02-14 02:08:05 -0500

    implemented archive folder to separate out processed files from files yet to be worked

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def main(args: list[str]) -> None:
 
     if len(args) == 0:
         pwd: Path = Path.cwd()
-        files, dest = selector(pwd)
+        files, dest, archive = selector(pwd)
         for file in files:
-            converter(file, dest)
+            converter(file, dest, archive)
         end_time = datetime.now()
         elapsed = time_elapsed(start_time, end_time)
         print('File conversions are complete')
@@ -31,17 +31,18 @@ def main(args: list[str]) -> None:
             raise Exception("File or directory not found")
 
         if arg.is_file():
-            dest: Path = make_destination_dir(arg.parent)
-            converter(arg, dest)
+            dest: Path = make_destination_dir(arg.parent, 'hevc')
+            archive: Path = make_destination_dir(parent, 'archive')
+            converter(arg, dest, archive)
             end_time = datetime.now()
             elapsed = time_elapsed(start_time, end_time)
             print('File conversions are complete')
             print(f'Time elapsed: {elapsed}\n')
 
         elif arg.is_dir():
-            files, dest = selector(arg)
+            files, dest, archive = selector(arg)
             for file in files:
-                converter(file, dest)
+                converter(file, dest, archive)
             end_time = datetime.now()
             elapsed = time_elapsed(start_time, end_time)
             print('File conversions are complete')
@@ -51,19 +52,20 @@ def main(args: list[str]) -> None:
         raise Exception('Please input only one(1) file or directory')
 
 
-def selector(target: Path) -> tuple[list[Path], Path]:
+def selector(target: Path) -> tuple[list[Path], Path, Path]:
     selection: str = input(f'Would you like to convert all files in {target}? y/N ')
 
     if selection.lower() == ("y" or "yes"):
-        dest: Path = make_destination_dir(target)
+        dest: Path = make_destination_dir(target, 'hevc')
+        archive: Path = make_destination_dir(target, 'archive')
         files: list[Path] = [f for f in target.iterdir() if f.is_file()]
-        return files, dest
+        return files, dest, archive
     else:
         exit(0)
 
 
-def make_destination_dir(directory: Path) -> Path:
-    destination: Path = directory.joinpath('hevc')
+def make_destination_dir(directory: Path, name: str) -> Path:
+    destination: Path = directory.joinpath(name)
 
     if not destination.exists():
         try:
@@ -92,8 +94,8 @@ def time_elapsed(start: datetime, end: datetime) -> str:
     return elapsed
 
 
-def log_event(timestamp: datetime, directory: Path, target: str, event_type: str) -> None:
-    log_file: Path = directory.joinpath('HEVC.log')
+def log_event(timestamp: datetime, parent_directory: Path, target: str, event_type: str) -> None:
+    log_file: Path = parent_directory.joinpath('HEVC.log')
 
     print(
         f'[{timestamp.date()} {timestamp.hour}:{timestamp.minute}:{timestamp.second}] {event_type} ({target})\n')
@@ -107,7 +109,7 @@ def log_event(timestamp: datetime, directory: Path, target: str, event_type: str
         exit(1)
 
 
-def converter(src: Path, dest: Path) -> None:
+def converter(src: Path, dest: Path, archive: Path) -> None:
     if src.suffix in ['.mp4', '.mkv', '.avi', '.mov', '.wmv']:
         new_name: str = f'{src.stem}.mp4'
         output_file: Path = dest.joinpath(new_name)
@@ -143,13 +145,18 @@ def converter(src: Path, dest: Path) -> None:
                 f'[{end_time.date()} {end_time.hour}:{end_time.minute}:{end_time.second}] Command {e.cmd} failed with error {e.returncode}')
             return
 
-        # if it isn't already HEVC, re-encode
+        # if it isn't already HEVC, re-encode, then put the original copy in the 'archive' directory
         try:
             result: subprocess.CompletedProcess = subprocess.run(
                 ['ffmpeg', '-i', src, '-c:v', 'hevc', '-c:a', 'copy', output_file],
                 stdout=subprocess.PIPE, check=True)
             print(result.stdout.decode())
-
+            try:
+                move(src, archive)
+            except FileExistsError:
+                end_time = datetime.now()
+                log_event(end_time, src.parent, src.name, 'FAILED TO ARCHIVE')
+                return
             end_time = datetime.now()
             log_event(end_time, src.parent, src.name, 'successfully converted')
             return
